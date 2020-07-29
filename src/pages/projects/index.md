3b3f26856455405ca127c36bc803d3370276d9a5
Stephen Kelehan <stephenkelehan@MacBook-Pro.local>
2020-07-29 16:42:30 +0100

    Adding projects page

--- src/pages/projects/index.md
+++ src/pages/projects/index.md
@@ -1,7 +1,21 @@
 ---
 templateKey: projects
 projectsPage:
-  title: Our Project
-  description: >-
-    Adipiscing vitae proin sagittis nisl rhoncus mattis. Bibendum enim facilisis gravida neque convallis a cras semper auctor. Sit amet risus nullam eget felis eget. Metus dictum at tempor commodo ullamcorper a lacus vestibulum. Sit amet facilisis magna etiam tempor orci eu. Eleifend mi in nulla posuere. Et magnis dis parturient montes nascetur ridiculus mus mauris vitae.
+  title: Our Latest Work
+  hero: ../../img/Notre-Studio2-02.png
+  projects:
+    - img: ../../img/One.png
+      link: https://hardcore-stonebraker-cc702f.netlify.app/
+    - img: ../../img/Two.png
+      link: https://affectionate-lamport-bee216.netlify.app/
+    - img: ../../img/Three.png
+      link: https://thirsty-roentgen-0bf08f.netlify.app/
+    - img: ../../img/Four.png
+      link: https://gallant-bartik-30cb5d.netlify.app/
+    - img: ../../img/Five.png
+      link: https://clever-blackwell-a7406f.netlify.app/
+    - img: ../../img/Six.png
+      link: https://happy-heyrovsky-186da5.netlify.app/
+    - img: ../../img/Seven.png
+      link: https://www.inshipaps.com/
 ---
--- src/templates/projects.js
+++ src/templates/projects.js
@@ -1,11 +1,118 @@
 import React from "react";
 import Layout from "../components/layout";
 import { graphql } from "gatsby";
+import Img from "gatsby-image";
+
+function projectsArray(array) {
+  var newArray = [];
+
+  for (let j = 0; j < array.length; j++) {
+    if (j % 2 === 1) {
+      continue;
+    } else {
+      if (array[j + 1]) {
+        newArray.push(
+          <>
+            <div className="row section-container-spacer">
+              <div className="col-md-6">
+                <a href={array[j].link} target="_blank">
+                  <Img
+                    className="img-responsive hover-website-link website-picture-links"
+                    alt=""
+                    fluid={array[j].img.childImageSharp.fluid}
+                  />
+                </a>
+              </div>
+
+              <div className="col-md-6">
+                <a
+                  href={array[j + 1].link}
+                  target="_blank"
+                  className="img-wrapper"
+                >
+                  <Img
+                    className="img-responsive inner-img hover-website-link website-picture-links"
+                    alt=""
+                    fluid={array[j + 1].img.childImageSharp.fluid}
+                  />
+                </a>
+              </div>
+            </div>
+          </>
+        );
+      } else if (array[j]) {
+        newArray.push(
+          <>
+            <div className="row section-container-spacer">
+              <div className="col-md-12">
+                <a
+                  href={array[j].link}
+                  target="_blank"
+                  className="website-container"
+                >
+                  <Img
+                    className="img-responsive hover-website-link website-picture-links"
+                    alt=""
+                    fluid={array[j].img.childImageSharp.fluid}
+                  />
+                </a>
+              </div>
+            </div>
+          </>
+        );
+      }
+    }
+  }
+
+  return newArray;
+}
 
 const projects = ({ data }) => {
+  const {
+    title,
+    hero,
+    projects,
+  } = data.markdownRemark.frontmatter.projectsPage;
+
+  console.log(data);
   return (
     <Layout current="pricing">
-      <div></div>
+      <div className="page-padding">
+        <div className="container">
+          <div className="row ">
+            <div className="col-lg-8 col-lg-offset-2 ">
+              <Img
+                className="img-responsive"
+                alt=""
+                fluid={hero.childImageSharp.fluid}
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="section-container">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <div className="text-center">
+                <h2>{title}</h2>
+              </div>
+              <div className="padding-top">{projectsArray(projects)}</div>
+            </div>
+
+            <div className="col-xs-12 col-md-8 col-md-offset-2">
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                Adipiscing commodo elit at imperdiet dui accumsan sit. Ipsum
+                dolor sit amet consectetur adipiscing elit.
+              </p>
+              <small className="signature pull-right">Nyasha & Stephen</small>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
@@ -17,7 +124,23 @@ export const pageQuery = graphql`
       frontmatter {
         projectsPage {
           title
-          description
+          hero {
+            childImageSharp {
+              fluid(maxWidth: 10000, quality: 100) {
+                ...GatsbyImageSharpFluid
+              }
+            }
+          }
+          projects {
+            img {
+              childImageSharp {
+                fluid(maxWidth: 10000, quality: 100) {
+                  ...GatsbyImageSharpFluid
+                }
+              }
+            }
+            link
+          }
         }
       }
     }
