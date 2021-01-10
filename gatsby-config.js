597cb07a4a509b39e454434cbdac089965adf8be
nyasharonaldo <nyasharonaldo@gmail.com>
2021-01-10 12:48:55 +0000

    Upgrading repo

--- gatsby-config.js
+++ gatsby-config.js
@@ -99,6 +99,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
+        icon: `src/img/Notre-Studio2-02.png`,
       },
     },
   ],
